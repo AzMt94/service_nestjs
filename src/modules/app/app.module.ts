0fe5ececd25aa59ff4402f684b5b0e6b7740bf52
AzMt94 <azamatmusinov@gmail.com>
2023-05-22 11:49:13 +0600

    -Create auth -Create methods login and register -Create JWT guard and JWT strategy -Create methods update abddelete user

---
+++ src/modules/app/app.module.ts
@@ -0,0 +1,40 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from '../user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import configurations from '../../configurations';
+import { User } from '../user/models/user.model';
+import { AuthModule } from '../auth/auth.module';
+import { TokenModule } from '../token/token.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configurations],
+    }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        dialect: 'postgres',
+        host: configService.get('db_host'),
+        username: configService.get('db_user'),
+        password: configService.get('db_password'),
+        port: configService.get('db_port'),
+        database: configService.get('db_name'),
+        synchronize: true,
+        autoLoadModels: true,
+        models: [User],
+      }),
+    }),
+    UserModule,
+    AuthModule,
+    TokenModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
